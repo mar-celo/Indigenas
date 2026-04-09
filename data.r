dc6c6fe8ad57086cb31c307691698317e02d19c0
Wesley de Jesus Silva <wesley.jesus@gestao.gov.br>
2026-04-09 20:42:02 -0300

    ajustes nos indicadores existentes: paleta e alguns valores

--- data.r
+++ data.r
@@ -1,5 +1,5 @@
 # ==============================================================================
-# 1. CONFIGURAÇÕES INICIAIS E BIBLIOTECAS
+# 1. CONFIGURAÇÕES INICIAIS E BIBLIOTECAS -------
 # ==============================================================================
 
 
@@ -9,29 +9,37 @@ library(dplyr)
 library(reticulate)
 library(sparklyr)
 library(stringr)
+library(lubridate)
+library(DBI)
+library(sparklyr)
+library(stringr)
+library(reticulate)
 
-pacotes <- c('arrow',
-             'dplyr',
-             'lubridate',
-             'DBI',
-             'sparklyr',
-             'stringr',
-             'reticulate'
-             )
+# pacotes <- c('arrow',
+#              'dplyr',
+#              'lubridate',
+#              'DBI',
+#              'sparklyr',
+#              'stringr',
+#              'reticulate'
+#              )
+#
+# for(i in pacotes){
+#   if(!require(i, character.only = TRUE )){
+#     install.packages(i, dependencies = TRUE)
+#     require(i, character.only = TRUE )
+#   }
+# }
 
-for(i in pacotes){
-  if(!require(i, character.only = TRUE )){
-    install.packages(i, dependencies = TRUE)
-    require(i, character.only = TRUE )
-  }
-}
+
+
 
 # objetos e diretório  --------------------------------------------------------
 dir.create("data",showWarnings = F)
 
 
 # ==============================================================================
-# 2. CONEXÃO COM O DATABRICKS
+# 2. CONEXÃO COM O DATABRICKS -------
 # ==============================================================================
 
 
@@ -55,11 +63,13 @@ df_spark <- spark_read_parquet(
 
 
 # ==============================================================================
-# 3. DATA WRANGLING (Spark + R)
+# 3. DATA WRANGLING (Spark + R) -------
 # ==============================================================================
 df_etnia <- df_spark %>%
   group_by(mes, nome_cor_origem_etnica) %>%
-  summarise(n = n(), .groups = "drop") %>%
+  summarise(linhas = n(),
+            n = sum(qtde_vinculos),
+            .groups = "drop") %>%
   collect() %>%
   mutate(
     # Limpeza de strings para garantir o match com a base
@@ -85,16 +95,16 @@ saveRDS(df_etnia,"data/df_etnia.rds")
 
 
 # ==============================================================================
-# 5. tabela4
+# 5. Tabela por UF -------
 # ==============================================================================
 
 
 # Filtrando apenas indígenas no último mês disponível (Fev 2026)
 # O processamento acontece no cluster, não no seu PC!
 df_indigenas_uf <- df_spark %>%
-  filter(mes == 202602, nome_cor_origem_etnica == "INDIGENA") %>%
-  group_by(uf) %>%
-  summarise(total_indigenas = n()) %>%
+  filter(mes == 202602) %>%
+  group_by(nome_cor_origem_etnica,uf) %>%
+  summarise(total_indigenas = sum(qtde_vinculos)) %>%
   collect() # Agora sim, trazemos apenas 27 linhas para o R
 
 
@@ -103,14 +113,14 @@ saveRDS(df_indigenas_uf,'data/df_indigenas_uf.rds')
 
 
 # ==============================================================================
-# 6. piramide
+# 6. piramide ------
 # ==============================================================================
 
 # 1. Agregação no Spark
 df_piramide <- df_spark %>%
   filter(mes == 202602) %>% # Filtro para o mês mais recente
   group_by(nome_faixa_etaria, faixa_etaria, nome_sexo) %>%
-  summarise(total = n(), .groups = "drop") %>%
+  summarise(total = sum(qtde_vinculos), .groups = "drop") %>%
   collect() %>%
   mutate(
     # Ajuste para criar os dois lados da pirâmide
@@ -118,26 +128,69 @@ df_piramide <- df_spark %>%
   )
 
 
+df_piramide_indigena <- df_spark %>%
+  # Filtro para o mês mais recente e para indígenas
+  filter(mes == 202602 & nome_cor_origem_etnica == "INDIGENA") %>%
+  group_by(nome_faixa_etaria, faixa_etaria, nome_sexo) %>%
+  summarise(total = sum(qtde_vinculos), .groups = "drop") %>%
+  collect() %>%
+  mutate(
+    # Ajuste para criar os dois lados da pirâmide
+    total_ajustado = ifelse(nome_sexo == "Fem", -total, total)
+  )
+
 ## salvando
 saveRDS(df_piramide,'data/df_piramide.rds')
+saveRDS(df_piramide_indigena,'data/df_piramide_indigena.rds')
 
 # ==============================================================================
 # 7. dados Treemap
 # ==============================================================================
 
 
+reorg_escol <-
+  c("ANALFABETO" =  "NÃO ALFABETIZADO",
+    "ENSINO FUNDAMENTAL INCOMPLETO"  = "ALFABETIZADO",
+    "ALFABETIZADO SEM CURSOS REGULA"  = "ALFABETIZADO",
+    "PRIMEIRO GRAU INCOMP.-ATE A 4A"  = "ALFABETIZADO",
+    "4A. SERIE DO PRIMEIRO GRAU COM"  = "ALFABETIZADO",
+    "ENSINO FUNDAMENTAL"  = "ENSINO FUNDAMENTAL",
+    "SEGUNDO GRAU INCOMPLETO"  = "ENSINO FUNDAMENTAL",
+    "SUPERIOR INCOMPLETO" = "ENSINO MÉDIO",
+    "ENSINO MEDIO" = "ENSINO MÉDIO",
+    "ENSINO SUPERIOR" = "ENSINO SUPERIOR",
+    "MESTRADO" = "MESTRADO",
+    "DOUTORADO" = "DOUTORADO"
+  )
+
+reorg_escol2 <-
+  c("Sem instrução" = "Não alfabetizado",
+    "Ensino Fundamental Incompleto" = "Alfabetizado",
+    "Ensino Fundamental Completo" = "Ensino Fundamental",
+    "Ensino Médio Incompleto" = "Ensino Fundamental",
+    "Ensino Médio Completo" = "Ensino Médio",
+    "Ensino Superior Incompleto" = "Ensino Médio",
+    "Ensino Superior Completo" = "Ensinio Superior",
+    "Pós-Graduação" = "Pós-Graduação")
+
+
 df_treemap_ind <- df_spark %>%
   filter(mes == 202602, nome_cor_origem_etnica == "INDIGENA") %>%
-  group_by(nome_escolaridade_completa, nome_regiao_politica) %>%
-  summarise(total = n(), .groups = "drop") %>%
+  group_by(nome_escolaridade_completa) %>%
+  summarise(total = sum(qtde_vinculos), .groups = "drop") %>%
   collect() %>%
   mutate(
-    nome_escolaridade_completa = str_trim(nome_escolaridade_completa),
-    # Abreviações para não poluir o gráfico
-    nome_escolaridade_completa = str_replace(nome_escolaridade_completa, "Ensino Superior Completo", "Superior"),
-    nome_escolaridade_completa = str_replace(nome_escolaridade_completa, "Pós-Graduação", "Pós"),
-    nome_regiao_politica = str_trim(nome_regiao_politica)
-  )
+    nome_escolaridade_new = reorg_escol2[str_trim(nome_escolaridade_completa)]
+  ) %>%
+  group_by(nome_escolaridade_new) %>%
+  summarise(total = sum(total), .groups = "drop")
+
+## reordenando
+df_treemap_ind <-
+  df_treemap_ind %>%
+  mutate(nome_escolaridade.f = factor(nome_escolaridade_new,
+                                      levels = unique(reorg_escol2),
+                                      ordered = T))
 
 
 saveRDS(df_treemap_ind,'data/df_treemap_ind.rds')
